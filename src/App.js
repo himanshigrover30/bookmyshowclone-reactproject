f7ef3bbe6ac9add0629d20322d1016a47e441c02
himanshigrover30 <himmigrover850@gmail.com>
2023-04-19 16:42:04 +0530

    Day 1 To 3

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+// React Slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Pages
+import HomePage from "./pages/Home.page";
+import MoviePage from "./pages/Movie.page";
+import PlayPage from "./pages/Play.page";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+    </Routes>
+  );
+}
+
+export default App;
