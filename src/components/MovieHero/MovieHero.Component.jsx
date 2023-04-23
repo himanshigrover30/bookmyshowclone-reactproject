2111c39bb17c1120124bc4b91e8bb32311b4c4b9
himanshigrover30 <himmigrover850@gmail.com>
2023-04-23 18:31:33 +0530

    MovieBannerAddedToMoviePage

--- src/components/MovieHero/MovieHero.Component.jsx
+++ src/components/MovieHero/MovieHero.Component.jsx
@@ -4,7 +4,25 @@ import { MovieContext } from "../../context/Movie.context";
 
 const MovieHero = () => {
   const { movie } = useContext(MovieContext);
-  return <div>MovieHero</div>;
+
+  const genres = movie.genres?.map(({ name }) => name).join(" , ");
+  //   console.log(genres);
+
+  return (
+    <>
+      <div>
+        {/* Mobile and Tab Screen i.e. small and medium screen size */}
+        <div className="lg:hidden w-full">
+          <img
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt="Cover Poster"
+            className="m-4 rounded"
+            style={{ width: "calc(100%-2rem)" }}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieHero;
