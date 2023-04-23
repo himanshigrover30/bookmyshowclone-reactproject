563c48587bb36e6266677754a57c4706ed4c2d74
himanshigrover30 <himmigrover850@gmail.com>
2023-04-23 12:32:18 +0530

    MovieNavbarStarted

--- src/context/Movie.context.jsx
+++ src/context/Movie.context.jsx
@@ -1,8 +1,19 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 export const MovieContext = createContext();
 
 const MovieProvider = ({ children }) => {
-  return <MovieContext.Provider>{children}</MovieContext.Provider>;
+  const [movie, setMovie] = useState({
+    id: 0,
+    original_title: "",
+    overview: "",
+    backdrop_path: "",
+    poster_path: "",
+  });
+  return (
+    <MovieContext.Provider value={{ movie, setMovie }}>
+      {children}
+    </MovieContext.Provider>
+  );
 };
 
 export default MovieProvider;
--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { MovieContext } from "../context/Movie.context";
 import MovieLayoutHoc from "../layout/Movie.layout";
+import Slider from "react-slick";
+import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
 
 const MoviePage = () => {
   const { id } = useParams();
+  const { movie, setMovie } = useContext(MovieContext);
   const [cast, setCast] = useState([]);
   const [similarMovies, setSimilarMovies] = useState([]);
   const [recommendedMovies, setRecommendedMovies] = useState([]);
@@ -36,7 +42,96 @@ const MoviePage = () => {
     requestRecommendedMovies();
   }, [id]);
 
-  return <div>MoviePage</div>;
+  useEffect(() => {
+    const requestMovie = async () => {
+      const getMovieData = await axios.get(`/movie/${id}`);
+      setMovie(getMovieData.data);
+    };
+    requestMovie();
+  }, [id]);
+
+  const settingsCast = {};
+  const settings = {};
+  return (
+    <>
+      <div className="my-12 container px-4 lg-ml-20 lg:w-2/1">
+        <div className="flex flex-col items-start gap-3">
+          <h1 className="text-gray-800 font-bold  gap-3 text-2xl">
+            About the movie
+          </h1>
+          <p>{movie.overview}</p>
+        </div>
+
+        <div className="my-8">
+          <hr />
+        </div>
+
+        <div className="my-8">
+          <h2 className="text-gray-800 font-bold text-2xl mb-3 ">
+            {" "}
+            Applicable offers
+          </h2>
+          <div className="flex flex-col gap-3 lg:flex-row ">
+            <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+              <div className="w-8 h-8">
+                <FaCcVisa className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-start">
+                <h3 className="text-gray-700 text-xl font-bold">
+                  Visa Stream Offers
+                </h3>
+                <p className="text-gray-700">
+                  Get 75% off up to INR 200 on all Rupay Card* on BookMyShow
+                  Stream
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+              <div className="w-8 h-8">
+                <FaCcApplePay className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-start">
+                <h3 className="text-gray-700 text-xl font-bold">
+                  Visa Stream Offers
+                </h3>
+                <p className="text-gray-700">
+                  Get 75% off up to INR 200 on all Rupay Card* on BookMyShow
+                  Stream
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-8">
+          <hr />
+        </div>
+
+        {/* Cast Sliders */}
+        <div className="my-8">
+          <PosterSlider
+            config={settings}
+            title="recommended movies"
+            posters={recommendedMovies}
+            isDark={false}
+          />
+        </div>
+
+        {/* Recommended movie Slider */}
+        <div className="my-8">
+          <hr />
+        </div>
+
+        <PosterSlider
+          config={settings}
+          title="BMS Xlusive Movies"
+          posters={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+    </>
+  );
 };
 
 export default MovieLayoutHoc(MoviePage);
